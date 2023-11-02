7e1d44a816ac6610d6c70f1063af382868b0f720
Haley Brokaw <haleybrokaw@gmail.com>
2023-11-02 10:18:00 -0600

    Added logic for asking for password length

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,17 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 var generateBtn = document.querySelector("#generate");
-
+function generatePassword(){
+  var length= prompt("How long would you like your password?")
+  if (length<8||length>128) {
+    alert("Length is out of bounds. Try again")
+    return
+  }
+  var wantsUppercase = confirm("Do you want uppercase characters?")
+  var wantsLowercase = confrim("Do you want lowercase characters?")
+  var wantsCharacters = confirm("Do you want special characters?")
+  var wantsNumbers = confirm("Do you want to add numbers?")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
